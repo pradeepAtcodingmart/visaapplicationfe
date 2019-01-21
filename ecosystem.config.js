eb812e70b5032cf6eb010ba71643669778870548
pradeepAtcodingmart <pradeep@codingmart.com>
2019-01-21 11:31:43 +0530

    pm2 installed

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps : [{
+    name: 'visappalication  ',
+    script: 'npm',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'run start:production',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    // env: {
+    //   NODE_ENV: 'development'
+    // },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '139.59.63.110',
+      ref  : 'origin/master',
+      repo : 'git@github.com:pradeepAtcodingmart/visaapplicationbe.git',
+      path : '/var/www/visaapplicationfe',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
