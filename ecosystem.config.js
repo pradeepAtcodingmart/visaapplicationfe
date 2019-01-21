6f3e5cf146ddda51a30a3fa26afd1093e2f6a862
pradeepAtcodingmart <pradeep@codingmart.com>
2019-01-21 13:59:10 +0530

    pm2 config changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,14 +4,10 @@ module.exports = {
     script: 'npm',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'run start:production',
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
-    // env: {
-    //   NODE_ENV: 'development'
-    // },
     env_production: {
       NODE_ENV: 'production'
     }
@@ -24,7 +20,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:pradeepAtcodingmart/visaapplicationfe.git',
       path : '/var/www/visaapplicationfe',
-      'post-deploy' : 'yarn install && yarn build'
+      'post-deploy' : 'yarn install && yarn build && pm2 stop all && yarn run prod:run'
     }
   }
 };
