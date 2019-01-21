53dc21449bc4dd2b323f361bd4eff532aaf20068
pradeepAtcodingmart <pradeep@codingmart.com>
2019-01-21 11:52:29 +0530

    config.js updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:pradeepAtcodingmart/visaapplicationbe.git',
       path : '/var/www/visaapplicationfe',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install'
     }
   }
 };
